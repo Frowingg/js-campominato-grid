8ed38b4780716a78106fe9185306dd071d3f19ef
Frowingg <edbico0@gmail.com>
2022-06-14 16:25:26 +0200

    campo-minato

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const gameDifficulty = 1;
-// parseInt(prompt('decidi la difficolta della partita (1-2-3)'))
-
+const gameDifficulty = parseInt(prompt('decidi la difficolta della partita (1-2-3)'))
+let game = true;
 let bombs = [];
-console.log(bombs)
+let userNumber;
+let j = 0;
 
 switch(gameDifficulty) {
     case 1:
@@ -18,19 +18,30 @@ switch(gameDifficulty) {
 
 function prodBombs(min, max) {
     for(i=1; i<=16; i++) {
-        num = getRndInteger(min, max)
+        num = getRndInteger(min, max);
         if (bombs.includes(num)) {
             i--;
         } else {
-            bombs.push(num)
+            bombs.push(num);
         }        
     }
 }
-console.log(bombs)
+console.log('bombs',bombs)
 
-
-
-
+while (game) {
+    userNumber = parseInt(prompt('dammi un numero'));
+    if (bombs.includes(userNumber)) {
+        alert('sei na pippa!')
+        game = false;
+    } else {
+        alert("c'hai preso!")
+        j++
+    }
+    if (j >= bombs.length) {
+        game = false
+        alert(`hai vinto con ${j}`)
+    }
+}
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
